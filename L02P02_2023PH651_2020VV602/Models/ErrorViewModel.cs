cb34d087cba9d7c456a8be33ed3e2fba2164ba11
Juan Hernández <jfph25@gmail.com>
2024-04-20 08:42:16 -0600

    Add project files.

---
+++ L02P02_2023PH651_2020VV602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2023PH651_2020VV602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
